84d66414f1b2f90cf70a090ba17af4291bca3988
Satyakumar BS <bs.satya@gmail.com>
2018-10-28 00:42:29 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Satyakumar BS on 28/10/18.
+//  Copyright © 2018 Passion Fruit Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
