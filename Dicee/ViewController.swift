1ad438357d9d7302b220c6ceb37fccdac5bcf18f
Satyakumar BS <bs.satya@gmail.com>
2018-10-28 13:05:43 +0530

    Added the Las Vegas Dice Roller app code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 1
+    
+    let diceImageArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceImageArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceImageArray[randomDiceIndex2])
+    }
 }
-
